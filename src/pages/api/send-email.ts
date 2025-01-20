fbf08f701bdaf32fbf1bef63dad2c98ac004e0d6
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-01-19 21:02:40 -0300

    send email route

--- src/pages/api/send-email.ts
+++ src/pages/api/send-email.ts
@@ -1,18 +1,18 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { EmailTemplate } from '../../components/email-template';
-import { resend } from '@/lib/resend';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { EmailTemplate } from '../../components/email-template'
+import { resend } from '@/lib/resend'
 
-export default  async (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { data, error } = await resend.emails.send({
     from: 'Acme <onboarding@resend.dev>',
     to: ['worktalles@hotmail.com'],
     subject: 'Hello world',
     react: EmailTemplate({ firstName: 'Talles Amaral' }),
-  });
+  })
 
   if (error) {
-    return res.status(400).json(error);
+    return res.status(400).json(error)
   }
 
-  res.status(200).json(data);
-};
+  res.status(200).json(data)
+}
