01627ecc3975aca252ba6bc868d90707afc90d94
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-01-26 18:41:20 -0300

    feat: add social media

---
+++ src/components/SocialMedia/style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+const SocialMediaContainer = styled.div`
+  ul {
+    li {
+      a {
+        display: inline-block;
+        svg {
+          transition: fill 0.3s ease;
+
+          &:hover {
+            fill: #fff; /* color on hover */
+          }
+        }
+      }
+    }
+  }
+`;
+
+export { SocialMediaContainer };
