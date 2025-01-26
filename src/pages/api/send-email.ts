1d5e949691856890a5e8b4666c270ae075196ba8
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-01-26 18:54:59 -0300

    feat: create a new route to send email

--- src/pages/api/send-email.ts
+++ src/pages/api/send-email.ts
@@ -1,18 +1,56 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { EmailTemplate } from '../../components/email-template'
 import { resend } from '@/lib/resend'
+import { z } from 'zod'
+import AdminEmail from '../../../emails/admin-email'
+import CustomerEmail from '../../../emails/customer-email'
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { data, error } = await resend.emails.send({
-    from: 'Acme <onboarding@resend.dev>',
-    to: ['worktalles@hotmail.com'],
-    subject: 'Hello world',
-    react: EmailTemplate({ firstName: 'Talles Amaral' }),
-  })
-
-  if (error) {
-    return res.status(400).json(error)
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' })
   }
 
-  res.status(200).json(data)
+  const token = req.headers.authorization
+
+  if (token !== `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`) {
+    return res.status(401).json({ error: 'Unauthorized' })
+  }
+
+  const sendEmailSchema = z.object({
+    name: z.string().min(3, { message: 'Name must be at least 3 characters' }),
+    email: z.string().email('This is not a valid email'),
+    message: z
+      .string()
+      .min(3, { message: 'Message must be at least 3 characters' }),
+  })
+
+  const { name, email, message } = sendEmailSchema.parse(req.body)
+
+  const customerEmailData = await resend.emails.send({
+    from: 'Contact | tallesamaral.dev <contact@tallesamaral.dev>',
+    to: ['tallesrafael60@gmail.com', email],
+    subject: 'Tranks for your message | Talles Amaral - Software Developer',
+    react: CustomerEmail({ name }),
+  })
+
+  const adminEmailData = await resend.emails.send({
+    from: 'Contact | tallesamaral.dev <contact@tallesamaral.dev>',
+    to: ['contact@tallesamaral.dev', 'tallesrafael60@gmail.com'],
+    subject: 'New Contact Form Submission',
+    react: AdminEmail({ name, email, message }),
+  })
+
+  if (customerEmailData.error || adminEmailData.error) {
+    return res.status(400).json({
+      error: 'Error sending email',
+    })
+  }
+
+  res.status(200).json({
+    message: 'Email sent successfully',
+    customerEmailData,
+    adminEmailData,
+  })
 }
