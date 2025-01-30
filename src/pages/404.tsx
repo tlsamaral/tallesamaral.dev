5a50ea8e4346cbc4c79ef457c74c39db09c40265
talles.amaral <worktalles@hotmail.com>
2025-01-30 11:10:29 -0300

    feat: 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+export default function Custom404() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+      <h1 className="text-2xl font-bold">404</h1>
+    </div>
+  )
+}
