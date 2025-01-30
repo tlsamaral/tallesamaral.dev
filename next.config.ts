cc2fa471f326f0325be183e06564c01d83eaeaaa
talles.amaral <worktalles@hotmail.com>
2025-01-30 10:58:09 -0300

    feat: allow images from cloudinary

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-};
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
