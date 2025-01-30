806140d6d93d7c7ff58bde616a1b5cb0c321e155
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-01-30 07:22:48 -0300

    feat: get content of db

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import Initial from '@/components/Initial/Initial'
 import AppProvider from '@/context/AppContext'
 import { Toaster } from 'sonner'
 import { GetServerSideProps } from 'next'
+import { prisma } from '@/lib/prisma'
 
 const Experiences = dynamic(
   () => import('../components/Experiences/Experiences'),
@@ -20,15 +21,45 @@ const Projects = dynamic(() => import('../components/Projects/Projects'), {
   ssr: false, // 🔥 Desativa SSR para evitar erro com GSAP
 })
 
-export default function Home() {
+export interface Project {
+  id: string
+  name: string
+  description: string
+  endDate: string
+  banner: string
+  status: boolean
+  repositoryUrl: string
+  appUrl: string
+  createdAt: string
+  tags: string[]
+}
+
+export interface Experience {
+  id: string
+  company: string
+  position: string
+  description: string
+  startDate: string
+  endDate: string
+  status: true
+  createdAt: string
+  tags: string[]
+}
+
+interface AppProps {
+  projects: Project[]
+  experiences: Experience[]
+}
+
+export default function Home({ experiences, projects }: AppProps) {
   return (
     <AppProvider>
       <Toaster richColors />
       <Header />
       <Initial />
       <AboutMe />
-      <Experiences />
-      <Projects />
+      <Experiences experiences={experiences} />
+      <Projects projects={projects} />
       <Contact />
       <Footer />
     </AppProvider>
@@ -36,7 +67,54 @@ export default function Home() {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
+  const projects = await prisma.project.findMany({
+    include: {
+      tags: {
+        select: {
+          tag: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  })
+
+  const serializedProjects = projects.map((project) => ({
+    ...project,
+    startDate: project.startDate ? project.startDate.toISOString() : null,
+    endDate: project.endDate ? project.endDate.toISOString() : null,
+    createdAt: project.createdAt.toISOString(),
+    tags: project.tags.map(({ tag }) => tag.name),
+  }))
+
+  const experiences = await prisma.experience.findMany({
+    include: {
+      tags: {
+        select: {
+          tag: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  })
+
+  const serializedExperiences = experiences.map((experience) => ({
+    ...experience,
+    startDate: experience.startDate ? experience.startDate.toISOString() : null,
+    endDate: experience.endDate ? experience.endDate.toISOString() : null,
+    createdAt: experience.createdAt.toISOString(),
+    tags: experience.tags.map(({ tag }) => tag.name),
+  }))
+
   return {
-    props: {},
+    props: {
+      projects: serializedProjects,
+      experiences: serializedExperiences,
+    },
   }
 }
