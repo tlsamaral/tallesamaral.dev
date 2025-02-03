53653be30c551c4485ded1f49a2237331b3efeb4
talles.amaral <worktalles@hotmail.com>
2025-02-03 14:18:35 -0300

    feat: add layouts

---
+++ src/layouts/index.tsx
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router'
+import { BackofficeLayout } from './backoffice'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const router = useRouter()
+
+  const isBackOffice = router.pathname.startsWith('/backoffice')
+
+  if (isBackOffice) {
+    return <BackofficeLayout>{children}</BackofficeLayout>
+  }
+
+  return children
+}
