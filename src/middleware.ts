aeb8402ae37c1801740c13fb3417de6b9569631b
talles.amaral <worktalles@hotmail.com>
2025-02-03 13:59:27 -0300

    feat: add ui components

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,29 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get('@tallesamaral:token') // Substitua pelo nome correto do seu cookie
-  const { pathname } = req.nextUrl
+	const token = req.cookies.get("@tallesamaral:token"); // Substitua pelo nome correto do seu cookie
+	const { pathname } = req.nextUrl;
 
-  // if (pathname === '/backoffice/sign-in') {
-  //   return NextResponse.next()
-  // }
+	// if (pathname === '/backoffice/sign-in') {
+	//   return NextResponse.next()
+	// }
 
-  // Se o usuário já estiver autenticado e tentar acessar /sign-in, redireciona para /backoffice
-  if (pathname === '/backoffice/sign-in' && token) {
-    return NextResponse.redirect(new URL('/backoffice', req.url))
-  } else if (pathname === '/backoffice/sign-in' && !token) {
-    return NextResponse.next()
-  }
+	// Se o usuário já estiver autenticado e tentar acessar /sign-in, redireciona para /backoffice
+	if (pathname === "/backoffice/sign-in" && token) {
+		return NextResponse.redirect(new URL("/backoffice", req.url));
+	} else if (pathname === "/backoffice/sign-in" && !token) {
+		return NextResponse.next();
+	}
 
-  // Se o usuário tentar acessar qualquer coisa dentro de /backoffice/ sem um token, redireciona para /login
-  if (pathname.startsWith('/backoffice') && !token) {
-    return NextResponse.redirect(new URL('/', req.url))
-  }
+	// Se o usuário tentar acessar qualquer coisa dentro de /backoffice/ sem um token, redireciona para /login
+	if (pathname.startsWith("/backoffice") && !token) {
+		return NextResponse.redirect(new URL("/", req.url));
+	}
 
-  return NextResponse.next() // Permite a requisição continuar normalmente
+	return NextResponse.next(); // Permite a requisição continuar normalmente
 }
 
 export const config = {
-  matcher: ['/backoffice/:path*', '/sign-in'], // Protege as rotas do backoffice e monitora /sign-in
-}
+	matcher: ["/backoffice/:path*", "/sign-in"], // Protege as rotas do backoffice e monitora /sign-in
+};
