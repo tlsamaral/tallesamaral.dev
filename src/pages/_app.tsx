f18098199b8b309782d78cf75e1e6eaf80b9d6ea
talles.amaral <worktalles@hotmail.com>
2025-02-04 15:07:31 -0300

    feat: add sonner toaster

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import Layout from '@/layouts'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Toaster } from 'sonner'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Toaster richColors />
     </Layout>
   )
 }
