a57780901dc053fa7d617a1f2229543a34cc624b
talles.amaral <worktalles@hotmail.com>
2025-02-04 17:41:59 -0300

    feat: add new components

--- src/components/Tag/style.ts
+++ src/components/Tag/style.ts
@@ -13,10 +13,15 @@ export const TagArea = styled.li`
 
   border-color: #9c6fa8;
   background-color: rgba(229, 170, 245, 0.2);
-
   color: #f0d2f8;
 
   box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
   backdrop-filter: blur(0.8px);
   -webkit-backdrop-filter: blur(0.8px);
+
+  @media (prefers-color-scheme: dark) {
+    border-color: rgb(77, 28, 88);
+    background-color: rgba(233, 195, 248, 0.4);
+    color: rgb(90, 28, 88);
+  }
 `
