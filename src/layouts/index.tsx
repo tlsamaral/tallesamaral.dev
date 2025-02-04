bec6fc715db02cf959dc5006389894519d016bab
talles.amaral <worktalles@hotmail.com>
2025-02-04 15:38:59 -0300

    feat: correct layouts

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -5,10 +5,20 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const router = useRouter()
 
   const isBackOffice = router.pathname.startsWith('/backoffice')
+  const isSignIn = router.pathname === '/backoffice/sign-in'
+  const isLandingPage = router.pathname === '/'
+
+  if (isLandingPage) {
+    return <div id="app">{children}</div>
+  }
+
+  if (isSignIn) {
+    return <>{children}</>
+  }
 
   if (isBackOffice) {
     return <BackofficeLayout>{children}</BackofficeLayout>
   }
 
-  return <div id="app">{children}</div>
+  return <>{children}</>
 }
