0f7e8b3da3ae65dad3cd0cbd34d2d2369664ceb1
talles.amaral <worktalles@hotmail.com>
2025-03-25 14:39:33 -0300

    feat: add seo config

---
+++ next-seo.config.ts
@@ -0,0 +1,41 @@
+// next-seo.config.js
+export default {
+  defaultTitle: 'Talles Amaral | Software Developer',
+  titleTemplate: '%s | Talles Amaral',
+  description:
+    'Conheça o portfólio de Talles Amaral, desenvolvedor de software especializado em criar soluções modernas, performáticas e inovadoras.',
+  canonical: 'https://www.tallesamaral.dev',
+  additionalMetaTags: [
+    {
+      name: 'author',
+      content: 'Talles Amaral',
+    },
+    {
+      name: 'keywords',
+      content:
+        'Talles Amaral, Software Developer, Desenvolvedor Web, Portfólio, Frontend, Backend, Desenvolvedor Fullstack, JavaScript, React, Next.js, Node.js, Programador',
+    },
+  ],
+  openGraph: {
+    type: 'website',
+    locale: 'pt_BR',
+    url: 'https://www.tallesamaral.dev',
+    siteName: 'Talles Amaral | Software Developer',
+    title: 'Talles Amaral | Software Developer',
+    description:
+      'Portfólio oficial de Talles Amaral, desenvolvedor especializado em soluções web modernas, rápidas e eficazes.',
+    images: [
+      {
+        url: 'https://tallesamaral.dev/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Portfólio Talles Amaral Software Developer',
+      },
+    ],
+  },
+  twitter: {
+    handle: '@tlsamaral_',
+    site: '@tlsamaral_',
+    cardType: 'summary_large_image',
+  },
+}
