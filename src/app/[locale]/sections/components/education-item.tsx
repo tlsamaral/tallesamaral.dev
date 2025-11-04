45c1c42f299bf34df4fefc62b15f2cd615aa6bc5
Talles <worktalles@hotmail.com>
2025-11-04 19:50:06 -0300

    chore: change education info

--- src/app/[locale]/sections/components/education-item.tsx
+++ src/app/[locale]/sections/components/education-item.tsx
@@ -5,11 +5,23 @@ import type { EducationItemProps } from '../education-section'
 export function EducationItem({ item }: { item: EducationItemProps }) {
   return (
     <div className="flex gap-4 items-start">
-      <Avatar className="size-14 border">
+      <Avatar className="size-14 border flex items-center justify-center overflow-hidden">
         <AvatarFallback className="uppercase">
           {getInitials(item.institution)}
         </AvatarFallback>
-        <AvatarImage src={item.avatarUrl || undefined} alt={item.institution} />
+        <AvatarImage
+          src={item.avatarUrl || undefined}
+          alt={item.institution}
+          width={32}
+          height={32}
+          className="
+            size-8
+            drop-shadow-sm
+            transition
+
+            dark:drop-shadow-[0_0_8px_rgba(255,255,255,0.1)]
+          "
+        />
       </Avatar>
       <div className="flex flex-col">
         <h4 className="font-bold">{item.degree}</h4>
