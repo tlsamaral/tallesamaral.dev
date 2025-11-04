041d59a77f50b6d2705e467875a99b81b19b080e
Talles <worktalles@hotmail.com>
2025-11-04 16:42:58 -0300

    chore: change number of items

--- src/app/[locale]/sections/components/accordion-skill-item.tsx
+++ src/app/[locale]/sections/components/accordion-skill-item.tsx
@@ -45,7 +45,7 @@ export function AccordionSkillItem({ category }: AccordionSkillItemProps) {
       </AccordionTrigger>
 
       <AccordionContent className="pt-2">
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2">
+        <div className="grid grid-cols-2 sm:grid-cols-3 gap-2">
           {category.stacks.map((stack) => (
             <div
               key={stack.name}
